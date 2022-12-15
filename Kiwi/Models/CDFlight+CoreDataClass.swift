3a20f7c9b9e0c58699f7757a28dbe10edfa1e1fa
DenisMihEpam <denis_mikhaylovskiy@epam.com>
2022-12-15 14:54:24 +0100

    file moved

--- Kiwi/Models/CDFlight+CoreDataClass.swift
+++ Kiwi/Models/CDFlight+CoreDataClass.swift
@@ -11,15 +11,5 @@ import CoreData
 
 
 public class CDFlight: NSManagedObject {
-    init(from model: Flight) {
-        self.id = model.id
-        self.cityFrom = model.cityFrom
-        self.cityTo = model.cityTo
-        self.countryTo = model.countryTo.name
-        self.created = Date()
-        self.dTime = Date(timeIntervalSince1970: Double(model.dTime))
-        self.distance = model.distance
-        self.price = Int32(model.price)
-        
-    }
+    
 }
